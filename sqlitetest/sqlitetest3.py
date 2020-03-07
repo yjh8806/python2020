e63d9a100e82127e786fd7a7e2edecf83332f422
yjh8806 <yjh8806@hotmail.com>
2020-03-07 13:44:18 +0900

    sqlite select문 연습

---
+++ sqlitetest/sqlitetest3.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+conn = sqlite3.connect('sqlitetest/example.db')
+c = conn.cursor()
+
+c.execute('select * from stocks') # * -> 와일드카드, 모든 것
+
+print(c.fetchone())               # select의 경우 insert처럼 commit필요 X 대신 fetch 필요
+print(c.fetchall())
+print(c.fetchmany(3))
+
+symbol = input("심볼을 입력하세요 : ")
+sql = "select * from stocks where symbol = '%s'" %symbol
+sql = "select * from stocks where symbol = ?"
+
+c.execute(sql, [symbol])          # symbol 리스트는 []
+# c.execute(sql, (symbol,))         symbol 튜플형인 경우 ','로 구분
+
+print(c.fetchall())
+conn.close()                      # commit은 필요 X, But close()는 해줘야 함
