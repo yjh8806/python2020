d4549f574f73171add1a28a20a596f237386b9b0
yjh8806 <yjh8806@hotmail.com>
2020-03-07 11:52:37 +0900

    sqlite insert문 연습

--- sqlitetest/sqlitetest2.py
+++ sqlitetest/sqlitetest2.py
@@ -5,8 +5,6 @@ c = conn.cursor()
 c.execute('''
 insert into stocks
 values('2020-01-05', 'buy', 'rhat', 100, 35.14)
-
-
 ''') # insert into 테이블명 values()
 
 conn.commit() # c.execute()에서 변화가 있었으므로, commit()해주어야 함
