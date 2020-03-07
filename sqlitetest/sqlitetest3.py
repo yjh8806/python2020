678255eccc517bf948f616722a7432e2c216627c
yjh8806 <yjh8806@hotmail.com>
2020-03-07 13:47:01 +0900

    sqlite select문 연습

--- sqlitetest/sqlitetest3.py
+++ sqlitetest/sqlitetest3.py
@@ -6,8 +6,8 @@ c = conn.cursor()
 c.execute('select * from stocks') # * -> 와일드카드, 모든 것
 
 print(c.fetchone())               # select의 경우 insert처럼 commit필요 X 대신 fetch 필요
+print(c.fetchmany(3))             # fetchone, fetchall 이후 fetchmany출력하면 리스트가 비어있음
 print(c.fetchall())
-print(c.fetchmany(3))
 
 symbol = input("심볼을 입력하세요 : ")
 sql = "select * from stocks where symbol = '%s'" %symbol
