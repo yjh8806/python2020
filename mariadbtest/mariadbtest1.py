0c600cab864cc6d2f29d066d6311bb980f7d51d4
yjh8806 <yjh8806@hotmail.com>
2020-03-08 10:38:52 +0900

    mariadb 테스트

--- mariadbtest/mariadbtest1.py
+++ mariadbtest/mariadbtest1.py
@@ -1,9 +1,21 @@
 import pymysql
 
-conn = pymysql.connect(host = 'localhost',
+conn = pymysql.connect(host = 'maria',
                        user = 'root',
                        password = 'qwer1234',
                        db = 'test',
                        cursorclass = pymysql.cursors.DictCursor)
 
-cur = conn.cursor()
+c = conn.cursor()
+c.execute('''
+create table if not exists stocks
+(date text, trans text, symbol text, qty real, price real)
+''')
+
+c.execute('''
+insert into stocks
+values('2020-01-05', 'buy', 'rhat', 100, 35.14)
+''')
+
+conn.commit() # c.execute()에서 변화(insert)가 있었으므로, commit()해주어야 함
+conn.close()
---
+++ mariadbtest/mariadbtest2.py
@@ -0,0 +1,16 @@
+import pymysql
+
+conn = pymysql.connect(host = 'maria',
+                       user = 'root',
+                       password = 'qwer1234',
+                       db = 'test',
+                       charset = 'utf8',
+                       cursorclass = pymysql.cursors.DictCursor
+                       )
+c = conn.cursor()                       
+sql = 'select * from stocks'
+
+c.execute(sql)
+
+print(c.fetchall())
+conn.close()
