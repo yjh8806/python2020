8225905e5329c9c513bc8f20d8736a61a935628a
yjh8806 <yjh8806@hotmail.com>
2020-03-08 12:03:01 +0900

    mariadb test

--- mariadbtest/mariadbtest4.py
+++ mariadbtest/mariadbtest4.py
@@ -8,7 +8,7 @@ conn = pymysql.connect(host = 'maria',
 
 c = conn.cursor()
 
-data = data = [
+data = [
     ('2020-01-05', 'buy', 'rhat', 200, 24.31),
     ('2020-02-17', 'buy', 'com', 100, 9.74),
     ('2020-05-27', 'buy', 'net', 55, 56.1),
@@ -20,7 +20,7 @@ sql = ('insert into stocks values(%s,%s,%s,%s,%s)')
 c.executemany(sql, data)
 conn.commit()   # insert문은 변화 有! commit 필수
 
-sql = ('select * from stocks order by price desc') # order by ~~: ~~순으로 정렬
+sql = 'select * from stocks order by price desc' # order by ~~: ~~순으로 정렬
 c.excute(sql)
 print(c.fetchall())
 conn.close()
