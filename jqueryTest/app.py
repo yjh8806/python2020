d15fa5198b24cb220c6e08856c2047506e7a6e3e
yjh8806 <yjh8806@hotmail.com>
2020-03-21 17:32:48 +0900

    200321 수업내용 updates(JS,JQuery)

--- jqueryTest/app.py
+++ jqueryTest/app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/index1')
+def index1():
+    return render_template('index1.html')
+
+@app.route('/index2')
+def index2():
+    return render_template('index2.html')
+
+@app.route('/index3')
+def index3():
+    return render_template('index3.html')
+
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0', port = 8890)
