d545e02765d483600a625bd13290ee59f9a5373d
yjh8806 <yjh8806@hotmail.com>
2020-03-22 15:53:57 +0900

    200322 updates

--- flask_CRUD_SQLAlchemy/App.py
+++ flask_CRUD_SQLAlchemy/App.py
@@ -5,34 +5,36 @@ app = Flask(__name__)
 app.secret_key = "Secret Key"
 
 #SqlAlchemy Database Configuration With Mysql
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:qwer1234@maria/test"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:qwer1234@maria/test" # mariadb 사용 시 URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # ↑ sqlite의 경우 경로 설정 -> 'sqlite:////tmp/test.db'
 
 db = SQLAlchemy(app)
 
-
+#---------------------------------테이블 정의
 #Creating model table for our CRUD database
-class Data(db.Model):
+class Data(db.Model): # 테이블 만드는 작업 (class를 통해서)
+                      # Data가 테이블 이름
+                      # id,name,email,phone : 필드 생성
+                      # (sql 쿼리문 create와 비교)
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(100))
     phone = db.Column(db.String(100))
 
-
+    # id 입력안해도 자동으로 
     def __init__(self, name, email, phone):
-
         self.name = name
         self.email = email
         self.phone = phone
-
+#---------------------------------테이블 정의
 
 
 #This is the index route where we are going to
 #query on all our employee data
 @app.route('/')
 def Index():
-    all_data = Data.query.all()
-
+    all_data = Data.query.all() # 테이블 Data = class Data
+                                # query.all() = select * from Data
     return render_template("index.html", employees = all_data)
 
 
@@ -48,9 +50,9 @@ def insert():
         phone = request.form['phone']
 
 
-        my_data = Data(name, email, phone)
-        db.session.add(my_data)
-        db.session.commit()
+        my_data = Data(name, email, phone) # 데이터 개체 생성(my_data)
+        db.session.add(my_data)            # 추가하고
+        db.session.commit()                # commit()까지
 
         flash("Employee Inserted Successfully")
 
@@ -62,6 +64,7 @@ def insert():
 def update():
 
     if request.method == 'POST':
+                # id값을 통해 값을 가져와 저장해둠
         my_data = Data.query.get(request.form.get('id'))
 
         my_data.name = request.form['name']
@@ -70,7 +73,7 @@ def update():
 
         db.session.commit()
         flash("Employee Updated Successfully")
-
+        
         return redirect(url_for('Index'))
 
 
---
+++ flask_member_SQLAlchemy/app.py
@@ -0,0 +1,226 @@
+from flask import Flask,request,render_template,redirect,url_for,jsonify
+import pymysql,os
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('bootstraptest.html')
+
+@app.route('/form')
+def formTest():
+    return render_template('form.html')
+
+@app.route('/formresult',methods=['POST'])  
+def formresult():
+    username = request.form['username']
+    userpass = request.form.get('userpass')
+    joblist=request.form.getlist('job')
+    return render_template('formresult.html',username=username,userpass=userpass,joblist=joblist) 
+
+@app.route('/bootstrap')
+def bootstraptest():
+    return render_template('bootstraptest.html')
+
+@app.route('/usersform',methods=['POST','GET'])
+def usersform():
+    if request.method == 'GET':
+        return render_template('usersform.html')   
+    else:
+        #print(type(request.values))
+        #for key in request.values:
+        #    print(key," : ",request.values[key])
+        #userid = request.form.get('userid')
+        #print('userid :',userid)
+        #for key in request.values:
+        #    eval(str(key)+"= '"+str(request.values[key])+"'")
+        userid = request.form.get('userid')
+        userpw = request.form.get('userpw')
+        username = request.form.get('username')
+        userage = request.form.get('userage')
+        useremail = request.form.get('useremail')
+        useradd = request.form.get('useradd')
+        usergender = request.form.get('usergender')
+        usertel = request.form.get('usertel')
+    
+        try:
+            connection=pymysql.connect(host='maria',
+                            user='root',
+                            password='qwer1234',
+                            db='test',
+                            charset='utf8mb4',
+                            cursorclass=pymysql.cursors.DictCursor)
+            
+            with connection.cursor() as cursor:
+                sql='''
+                    insert into users values(%s,%s,%s,%s,%s,%s,%s,%s);
+                    '''
+                cursor.execute(sql,(userid,userpw,username,userage,useremail,useradd,usergender,usertel))
+                connection.commit()
+                     
+        finally:
+            connection.close()                            
+
+    return redirect('/list')
+
+@app.route('/updateform/<userid>',methods=['GET'])
+def updateformget(userid):
+    connection=pymysql.connect(host='maria',
+                            user='root',
+                            password='qwer1234',
+                            db='test',
+                            charset='utf8mb4',
+                            cursorclass=pymysql.cursors.DictCursor)
+       
+    try:
+        with connection.cursor() as cursor:
+            sql="select * from users where userid = %s;"
+            cursor.execute(sql,userid)
+            result=cursor.fetchone()
+            print(result)
+    finally:
+        connection.close()
+    return render_template('updateform.html',list=result)  
+
+@app.route('/updateform',methods=['POST'])
+def updateformpost():
+    connection=pymysql.connect(host='maria',
+                            user='root',
+                            password='qwer1234',
+                            db='test',
+                            charset='utf8mb4',
+                            cursorclass=pymysql.cursors.DictCursor)
+
+    userid = request.form.get('userid')
+    userpw = request.form.get('userpw')
+    username = request.form.get('username')
+    userage = request.form.get('userage')
+    useremail = request.form.get('useremail')
+    useradd = request.form.get('useradd')
+    usergender = request.form.get('usergender')
+    usertel = request.form.get('usertel')
+    
+    try:
+        with connection.cursor() as cursor:
+            sql='''
+                update users 
+                set 
+                userpw=%s,
+                username=%s,
+                userage=%s,
+                usermail=%s,
+                useradd=%s,
+                usergender=%s,
+                usertel=%s
+                where userid=%s;
+                '''
+            cursor.execute(sql,(userpw,username,userage,useremail,useradd,usergender,usertel,userid))
+            connection.commit()
+    finally:
+        connection.close()                            
+    return redirect('/list')    
+
+@app.route('/content/<userid>')
+def content(userid):
+    connection=pymysql.connect(host='maria',
+                            user='root',
+                            password='qwer1234',
+                            db='test',
+                            charset='utf8mb4',
+                            cursorclass=pymysql.cursors.DictCursor)
+       
+    try:
+        with connection.cursor() as cursor:
+            sql="select * from users where userid = %s;"
+            cursor.execute(sql,userid)
+            result=cursor.fetchone()
+            print(result)
+    finally:
+        connection.close()
+    return render_template('content.html',list=result)
+
+
+@app.route('/deleteform/<userid>')
+def deleteformget(userid):
+    connection=pymysql.connect(host='maria',
+                            user='root',
+                            password='qwer1234',
+                            db='test',
+                            charset='utf8mb4',
+                            cursorclass=pymysql.cursors.DictCursor)
+       
+    try:
+        with connection.cursor() as cursor:
+            sql="delete from users where userid = %s;"
+            cursor.execute(sql,userid)
+            connection.commit()
+    finally:
+        connection.close()
+    return redirect('/list')  
+
+@app.route('/list')
+def list():
+    connection=pymysql.connect(host='maria',
+                            user='root',
+                            password='qwer1234',
+                            db='test',
+                            charset='utf8mb4',
+                            cursorclass=pymysql.cursors.DictCursor)
+    try:
+        with connection.cursor() as cursor:
+                sql="select * from users;"
+                cursor.execute(sql)
+                result=cursor.fetchall()
+                print(result)
+    finally:
+            connection.close()
+    return render_template('list.html',list=result)      
+
+@app.route('/ajaxlist',methods=['GET'])
+def ajaxlistget():
+    connection=pymysql.connect(host='maria',
+                            user='root',
+                            password='qwer1234',
+                            db='test',
+                            charset='utf8mb4',
+                            cursorclass=pymysql.cursors.DictCursor)
+    try:
+        with connection.cursor() as cursor:
+                sql="select  * from users;"
+                cursor.execute(sql)
+                result=cursor.fetchall()
+                print(result)
+    finally:
+            connection.close()
+    return render_template('ajaxlist.html',list=result)      
+
+@app.route('/ajaxlist',methods=['POST'])
+def ajaxlistpost():
+    userid = request.form.get('userid')
+    connection=pymysql.connect(host='maria',
+                            user='root',
+                            password='qwer1234',
+                            db='test',
+                            charset='utf8mb4',
+                            cursorclass=pymysql.cursors.DictCursor)
+    try:
+        with connection.cursor() as cursor:
+                sql="select * from users where userid like %s;"
+                userid='%' + userid+'%' # yjh8806을 y,j,h 중 꼭 y만이 아니라 다른것을 사용해서 찾을 수 있게 하려면 '%' + userid + '%'
+                cursor.execute(sql,userid)
+                result=cursor.fetchall()
+                print(result)
+    finally:
+            connection.close()
+    return jsonify(result)    
+
+@app.route('/imglist')
+def imglist():
+    print(os.path.dirname(__file__))
+    dirname=os.path.dirname(__file__) + '/static/img/'
+    filenames = os.listdir(dirname)
+    print(filenames)
+    return render_template('imglist.html',filenames=filenames)        
+
+if __name__ =='__main__':
+    app.run(debug=True,host='0.0.0.0',port=8890)
