fd8019068387de41a204948e5d8887247380e6e6
yjh8806 <yjh8806@hotmail.com>
2020-04-12 11:13:55 +0900

    200412 checks

---
+++ 200411_WebCrawling/flask_movie_SQLAlchemy/.ipynb_checkpoints/userdb-checkpoint.sql
@@ -0,0 +1,28 @@
+﻿create database if not exists member;
+commit;
+use member;
+drop table if exists users;
+create table if not exists users(
+userid varchar(20) primary key,
+userpw varchar(20) not null,
+username varchar(20) not null,
+userage int,
+usermail varchar(20),
+useradd varchar(50),
+usergender varchar(20),
+usertel varchar(20));
+
+
+insert into users values(
+'hong',
+'1234',
+'홍길동',
+23,
+'hong@gmail.com',
+'부산시 동구 수정동',
+'male',
+'010-1234-1234');
+
+select * from users;
+
+select version();
